7312d609b28b724557c6dab6f86006dd465da60e
Vít Chrubasík <vit@chrubasik.cz>
2023-06-14 14:40:37 +0200

    update nvim conf

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,7 @@
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.wo.relativenumber = true
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
