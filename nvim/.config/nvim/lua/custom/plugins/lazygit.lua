98b42fd87591c51d4790c7aa02e7be1adc8ce64e
Vít Chrubasík <vit@chrubasik.cz>
2023-11-21 18:36:36 +0100

    add lazygit to nvim

---
+++ nvim/.config/nvim/lua/custom/plugins/lazygit.lua
@@ -0,0 +1,15 @@
+-- nvim v0.8.0
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("lazygit")
+      vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>',
+        { silent = true, noremap = true, desc = '[G]o to Lazy[G]it' })
+    end,
+  },
+}
