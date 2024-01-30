f59ff4442ccaf3079866e752bf625636a3610a95
Vít Chrubasík <vit@chrubasik.cz>
2024-01-30 19:42:34 +0100

    ignore missing fields

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -248,6 +248,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
+---@diagnostic disable-next-line: missing-fields
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'sql' },
--- nvim/.config/nvim/lua/custom/config/cmp_config.lua
+++ nvim/.config/nvim/lua/custom/config/cmp_config.lua
@@ -5,6 +5,7 @@ local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
+---@diagnostic disable-next-line: missing-fields
 cmp.setup({
   snippet = {
     expand = function(args)
