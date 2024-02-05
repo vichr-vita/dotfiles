a6f2fce4ee1e7b6949424a967739e52212f5c911
Vit Chrubasik <vit@chrubasik.cz>
2024-02-05 17:58:18 +0100

    chore(nvim): mypy now checks envs before global

--- nvim/.config/nvim/lua/custom/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/custom/plugins/null-ls.lua
@@ -4,14 +4,20 @@ return {
     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     local null_ls = require("null-ls")
 
-
     local opts = {
       sources = {
         -- js/ts
         -- null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.prettier,
         -- python
-        null_ls.builtins.diagnostics.mypy,
+        -- null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.mypy.with({
+          extra_args = function()
+            local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+            return { "--python-executable", virtual .. "/bin/python3" }
+          end,
+        }),
+
         -- null_ls.builtins.diagnostics.ruff,
       },
       -- the on attach formatting is handled in autoformat.lua
