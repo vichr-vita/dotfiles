c68239893bde1ac6c432608246b606d80df4c8ec
Vít Chrubasík <me@vichr.me>
2025-01-26 19:22:37 +0100

    refactor(telescope): put fzf native as dep

--- nvim/.config/nvim/lua/vichr/plugins/telescope.lua
+++ nvim/.config/nvim/lua/vichr/plugins/telescope.lua
@@ -1,2 +1,15 @@
 -- Fuzzy Finder (files, lsp, etc)
-return { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } }
+return {
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      -- NOTE: If you are having trouble with this installation,
+      --       refer to the README for telescope-fzf-native for more instructions.
+      build = 'make',
+    }
+
+  }
+}
